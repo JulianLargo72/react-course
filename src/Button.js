01a46204d90f3f8fe3619fb6dd48d4bdd944cf76
JulianLargo72 <julianbetancur104@gmail.com>
2024-06-04 16:26:54 -0500

    second commit

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,17 @@
-export function Button() {
-    return <button>
-        Mi boton
+import PropTypes from 'prop-types'
+
+export function Button({text, name}) {
+    return <button onClick={function () {
+        console.log('Hola Mundo')
+    }}>
+        {text} - {name}
     </button>
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired
+}
+
+Button.defaultProps = {
+    name: 'Some User'
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { Greeting, UserCard } from "./Greeting";
-import Product, { NavBar } from "./Product";
-import {Button} from './Button'
-
+import { Posts } from "./Posts";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Counter() {
+  const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
+
+  return (
+    <div>
+      <input onChange={(e) => setMensaje(e.target.value)} />
+      <button
+        onClick={() => {
+          alert("El mensaje es:" + mensaje);
+        }}
+      >
+        save
+      </button>
+      <hr/>
+        <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>
+        Incrementar
+      </button>
+    </div>
+  );
+}
 
 root.render(
   <>
-    
-    <UserCard
-      name="Ryan Ray"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () { alert('Hello') }}
-    />
-    <UserCard
-    name="Joe Mcmillan"
-    amount={1000}
-    married={false}
-    points={[100, 20]}
-    address={{
-        street: "av some 123",
-        city: 'new york'
-    }}
-    />
+    <Counter />
   </>
 );
